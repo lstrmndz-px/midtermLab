7f6a59bf0e321696d4b97b2208468872991696bc
Lester R. Mendoza <lstrmndz10@gmail.com>
2024-07-05 11:02:30 +0800

    Update file1.py
    fix the bug

--- file1.py
+++ file1.py
@@ -12,4 +12,4 @@ while len(sequence) < n:
 sequence.append(sequence[-1] + sequence[-2])
 return sequence
 result = fibonacci_sequence(10)
-print(f"Fibonacci sequence up to 10 numbers: {result}")
+printf("Fibonacci sequence up to 10 numbers: {result}")
